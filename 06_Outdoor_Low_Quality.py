9abb9c574f543fe1dc121acd671f967edd623f21
rjngerd74bghf <129916783+rjngerd74bghf@users.noreply.github.com>
2023-04-08 14:58:10 -0800

    Update 06_Outdoor_Low_Quality.py

--- 06_Outdoor_Low_Quality.py
+++ 06_Outdoor_Low_Quality.py
@@ -2,6 +2,7 @@ import bpy
 scene = bpy.context.scene
 
 
+
 scene.pov.radio_display_advanced = True
 scene.pov.radio_adc_bailout = 0.005
 scene.pov.radio_always_sample = False
@@ -23,5 +24,4 @@ scene.pov.radio_normal = False
 
 scene.pov.radio_recursion_limit = 1
 scene.pov.radio_pretrace_start = 0.08
-
 scene.pov.radio_pretrace_end = 0.01
